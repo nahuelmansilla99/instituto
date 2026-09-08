17ae54ad9a6a2f6469be086023d86842bf86b0f1
Nahuel Mansilla <nahuel.mansilla1999@gmail.com>
2026-09-08 09:18:51 -0300

    [fix] Manejo de tokens expirados

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -23,7 +23,7 @@ import { User } from '../entities/user.entity';
         return {
           secret,
           signOptions: {
-            expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '7d',
+            expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '24h',
           },
         };
       },
